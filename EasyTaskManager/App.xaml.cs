4503ea2e411ed5b207501f0919d9c752d960145a
DShalygin <DShalygin@it.ru>
2018-05-18 17:06:55 +0700

    Common changes
    Single instance application (mutex)
    Add logging

--- EasyTaskManager/App.xaml.cs
+++ EasyTaskManager/App.xaml.cs
@@ -1,5 +1,9 @@
-﻿using EasyTaskManager.ViewModels.MainViewModel;
+﻿using System.Diagnostics;
+using System.Reflection;
+using System.Threading;
+using EasyTaskManager.ViewModels.MainViewModel;
 using System.Windows;
+using Microsoft.VisualBasic.Logging;
 using UseAbilities.MVVM.Managers;
 
 namespace EasyTaskManager
@@ -11,8 +15,34 @@ namespace EasyTaskManager
     {
         private void App_OnStartup(object sender, StartupEventArgs e)
         {
+            InitLogger();
+
+            // Use the assembly GUID as the name of the mutex which we use to detect if an application instance is already running
+            bool isApplicationRunning;
+            var mutex = new Mutex(false, Assembly.GetExecutingAssembly().GetType().GUID.ToString(), out isApplicationRunning);
+            Debug.WriteLine("isApplicationRunning = {0}", isApplicationRunning);
+            if (!isApplicationRunning)
+            {
+                Debug.WriteLine(" Current.Shutdown()");
+                // Only allow one instance
+                Current.Shutdown();
+            }
+
             AdvancedViewManager.Instance.RegisterRelation<MainViewModel, Views.MainView>();
             AdvancedViewManager.Instance.ResolveAndShow<MainViewModel>();
+        }
+
+        private void InitLogger()
+        {
+            Debug.Listeners.Add(
+                new FileLogTraceListener
+                    {
+                        Location = LogFileLocation.ExecutableDirectory,
+                        BaseFileName = "etm_",
+                        Append = true,
+                        Delimiter = "|",
+                        AutoFlush = true
+                });
         }
     }
 }
